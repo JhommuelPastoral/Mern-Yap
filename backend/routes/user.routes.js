332c5bb18e9b86aaf8fdf5d3490649940e30e18a
JhommuelPastoral <jhommuelpastoral9@gmail.com>
2025-04-18 08:27:22 +0800

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { login, signUp } from '../controllers/users.controller.js';
 const userRoutes = express.Router();
 
-userRoutes.get('/login', login);
+userRoutes.post('/login', login);
 userRoutes.post('/signUp', signUp)
 
 export default userRoutes;
